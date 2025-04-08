79d31e37c1fd82904aa2e35d12c66b52d97c0a10
Triger122 <liortrigerr@gmail.com>
2025-04-08 11:02:21 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1 style={{ color: 'green' }}>🎉 ברוך הבא ל-HireMe!</h1>
+      <p>האתר פועל בהצלחה!</p>
+    </div>
+  );
+}
+
+export default App;
